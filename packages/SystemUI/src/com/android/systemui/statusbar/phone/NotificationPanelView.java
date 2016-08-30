7d5e601542e6f22cc89c667149a131f7cc378a19
dooyoung.hwang <dooyoung.hwang@lge.com>
2016-08-30 18:43:48 +0900

    Also block touches while customizer is animating.
    If user swipe up while while customizer is animating, QSTracking
    occurs and customizer could go to beyond screen. After that user
    cannot drag down NotificationPanel even after closing customizer.
    
    Change-Id: I40a591909154db02724bf6acec09f2dee6d9aad1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -608,7 +608,7 @@ public class NotificationPanelView extends PanelView implements
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
-        if (mBlockTouches || mQsContainer.isCustomizing()) {
+        if (mBlockTouches || mNotificationContainerParent.isCustomizerShowingOrAnimating()) {
             return false;
         }
         initDownStates(event);
@@ -768,7 +768,7 @@ public class NotificationPanelView extends PanelView implements
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (mBlockTouches || mQsContainer.isCustomizing()) {
+        if (mBlockTouches || mNotificationContainerParent.isCustomizerShowingOrAnimating()) {
             return false;
         }
         if (mDoubleTapToSleepEnabled
