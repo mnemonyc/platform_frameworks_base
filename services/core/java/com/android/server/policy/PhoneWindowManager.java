0aedd0e84e68194c1359c61468b3421efd0ec23b
mfonville <maarten.fonville@gmail.com>
2016-09-26 13:17:56 +0200

    webview_packages: prefer the more powerful webview packages
    If Gapps are installed, better webview providers
    become avaliable.
    
    This commit adds support for those and makes the system
    choose the best provider: Chrome Stable>Google Webview>AOSP Webview
    
    It also allows the user to choose between additional
    providers from Play Store: Chrome Beta, Chrome Dev and Chrome Canary.
    
    AOSP WebView is used as Fallback in case no Gapps were installed.
    If Gapps were detected, AOSP WebView will be blocked and cannot be
    chosen any more.
    
    This patch is a collobarative work of:
    
    mfonville <maarten.fonville@gmail.com> @mfonville
    rapperskull <rapper.skull@hotmail.it> @rapperskull
    Arne-Christian Blystad <arne.christian.blystad@bartec-pixavi.com> @Blystad
    Alex Naidis <alex.naidis@linux.com> @TheCrazyLex
    
    You can find more information on this topic here: opengapps/aosp_build#76
    
    Change-Id: I3beecf38c75600ec05c9018259dfe3b539b73614
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7188,6 +7188,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             }
 
+            case KeyEvent.KEYCODE_HOME:
+                if (down && !interactive && mHomeWakeScreen) {
+                    isWakeKey = true;
+                }
+                break;
             case KeyEvent.KEYCODE_ENDCALL: {
                 result &= ~ACTION_PASS_TO_USER;
                 if (down) {
