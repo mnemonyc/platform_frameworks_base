440fc0b36bb3fb06e8b5810de71418dbe7e1be89
ezio84 <brabus84@gmail.com>
2017-01-12 11:18:56 +0800

    Battery icon improvements 1) on stock aosp, for landscape and portrait battery icon mode, pct and bolt are white when the battery level is less or equal to 30%, and this is good because then they are more visible in the battery icon.
    2) move back the critical level symbol ! out of the pctText related
    if else so it will be always displayed in the icon when the level is
    <= mCriticalLevel (it's good to warn the user that level is critical
    also if he doesn't want pct inside the icon)
    
    3) change the boltOverlay bool name to an easy-to-understand one
    
    4) on landscape or portrait icon mode, when level is <=15, the pct
    inside the icon is red. Before this commit, though, the XOR porterduff
    mode was applied, rather now let's do what we did in the point (1) of
    this commit for the white pct: make the pct full red and don't care of
    statusbar colorization, so the pct will be more visible.
    It will be full white if power saver enabled because getColorForLever
    provides the needed code.
    
    5) If the user sets no pct inside the icon, show a "+" symbol inside
    the icon when power save mode is enabled.
    PS: made a vector for the "+", the string looks like shit
    
    6) remove aosp leftovers we never used
    
    7) code cleanup
    
    8)unlink the pct/charge level red color from the powersave mode color
    so we can use a different one. Set it to #E53935 (material red 600) so
    it's better visible on the dark grey frame color
    Preview: https://s28.postimg.org/awbopifu5/6424563064165135416_account_id_1.png
    
    Conflicts:
    	packages/SystemUI/res/values/arrays.xml
    	packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -39,7 +39,6 @@ public class BatteryMeterView extends ImageView implements
     private BatteryController mBatteryController;
 
     private final Context mContext;
-    private final int mFrameColor;
 
     public BatteryMeterView(Context context) {
         this(context, null, 0);
@@ -51,13 +50,7 @@ public class BatteryMeterView extends ImageView implements
 
     public BatteryMeterView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-
-        TypedArray atts = context.obtainStyledAttributes(attrs, R.styleable.BatteryMeterView,
-                defStyle, 0);
-        final int frameColor = atts.getColor(R.styleable.BatteryMeterView_frameColor,
-                context.getColor(R.color.batterymeter_frame_color));
-        mDrawable = new BatteryMeterDrawable(context, new Handler(), frameColor);
-        atts.recycle();
+        mDrawable = new BatteryMeterDrawable(context, new Handler());
 
         setImageDrawable(mDrawable);
 
@@ -66,7 +59,6 @@ public class BatteryMeterView extends ImageView implements
         setLayerType(View.LAYER_TYPE_HARDWARE, null);
 
         mContext = context;
-        mFrameColor = frameColor;
     }
 
     @Override
@@ -120,7 +112,7 @@ public class BatteryMeterView extends ImageView implements
                 setImageDrawable(null);
                 break;
             default:
-                mDrawable = new BatteryMeterDrawable(mContext, new Handler(), mFrameColor, style);
+                mDrawable = new BatteryMeterDrawable(mContext, new Handler(), style);
                 setImageDrawable(mDrawable);
                 setVisibility(View.VISIBLE);
                 break;
@@ -134,7 +126,7 @@ public class BatteryMeterView extends ImageView implements
         if (style == BatteryMeterDrawable.BATTERY_STYLE_TEXT || style == BatteryMeterDrawable.BATTERY_STYLE_HIDDEN) {
             return;
         } else {
-        mDrawable = new BatteryMeterDrawable(mContext, new Handler(), mFrameColor, style);
+        mDrawable = new BatteryMeterDrawable(mContext, new Handler(), style);
         setImageDrawable(mDrawable);
         setVisibility(View.VISIBLE);
         restoreDrawableAttributes();
