3b4435cb596c3f0019fd3a1e6e00cb87ebd5d1cd
ezio84 <brabus84@gmail.com>
2017-01-29 16:33:36 +0100

    Fix recents thumbnails for one hand mode
    Before this the thumbnail of the foreground app in the recents view
    was resized in the thumbnail view itself (One Hand Mode Inception :V)
    
    Change-Id: I5f6b9558d9a975bc314d47bcab13f8b216283433

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskViewThumbnail.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskViewThumbnail.java
@@ -171,24 +171,19 @@ public class TaskViewThumbnail extends View {
             int w = bm.getWidth();
             Bitmap cropped = null;
 
-            int mode = currentHandsMode();
-            try {
-                if (mode == 1) {
-                    cropped = Bitmap.createBitmap(bm, 0, (int)(h * (1-INITIAL_SCALE)),
-                            (int)(w * INITIAL_SCALE), (int)(h * INITIAL_SCALE));
-                } else if (mode == 2) {
-                    cropped = Bitmap.createBitmap(bm, (int)(w * (1-INITIAL_SCALE)), (int)(h * (1-INITIAL_SCALE)),
-                            (int)(w * INITIAL_SCALE), (int)(h * INITIAL_SCALE));
-                }
-            } catch (Exception e) {
-                cropped = bm;
+            if (currentHandsMode() == 1) {
+                cropped = Bitmap.createBitmap(bm, 0, (int)(h * (1-INITIAL_SCALE)),
+                        (int)(w * INITIAL_SCALE), (int)(h * INITIAL_SCALE));
+            } else if (currentHandsMode() == 2) {
+                cropped = Bitmap.createBitmap(bm, (int)(w * (1-INITIAL_SCALE)), (int)(h * (1-INITIAL_SCALE)),
+                        (int)(w * INITIAL_SCALE), (int)(h * INITIAL_SCALE));
             }
 
-            mBitmapShader = new BitmapShader(mode != 0 ? cropped: bm,
+            mBitmapShader = new BitmapShader(currentHandsMode() != 0 ? cropped: bm,
                     Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
             mDrawPaint.setShader(mBitmapShader);
-            mThumbnailRect.set(0, 0, mode != 0 ? cropped.getWidth() : w,
-                    mode != 0 ? cropped.getHeight() : h);
+            mThumbnailRect.set(0, 0, currentHandsMode() != 0 ? cropped.getWidth() : w,
+                    currentHandsMode() != 0 ? cropped.getHeight() : h);
             mThumbnailInfo = thumbnailInfo;
             updateThumbnailScale();
         } else {
