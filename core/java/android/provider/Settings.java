492d79b27849eae46f78137c97a6cd542580cb99
Salvatore Festa <salvatorefestatech@gmail.com>
2017-02-10 11:09:08 +0100

    Added missing strings and removed duplicated strings

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3788,13 +3788,7 @@ public final class Settings {
          * @hide
          */
         public static final String KEYGUARD_TOGGLE_TORCH = "keyguard_toggle_torch";
-
-        /**
-         * whether to enable torch on lockscreen
-         * @hide
-         */
-        public static final String KEYGUARD_TOGGLE_TORCH = "keyguard_toggle_torch";
-
+        
         /**
          * Show or hide clock
          * 0 - hide
