91be5685334b809858d604b07730dd4961fdbf05
Salvatore Festa <salvatorefestatech@gmail.com>
2017-04-04 11:16:03 +0200

    Fix issue with saving admins before finishing loading.
    Saving device policy managers settings to clear out
    password stats was happening before initializing mAdminList
    so could wipe active admins.
    
    Test: manual - flash with N2G05C add google account with dmagent flash wth this fix, check dmagent is still an active admin, reboot check admin is still active.
    Test: runtest -c com.android.server.devicepolicy.DevicePolicyManagerTest frameworks-services
    
    Bug: 34277435
    Change-Id: I13660b47f30e9aba001eb13f2e457c3b3f36da3e
    (cherry picked from commit adbda7474cc1968b66e9948aee566dc346e71340)

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -2498,11 +2498,6 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
             // Ignore
         }
 
-        // Might need to upgrade the file by rewriting it
-        if (needsRewrite) {
-            saveSettingsLocked(userHandle);
-        }
-
         // Generate a list of admins from the admin map
         policy.mAdminList.addAll(policy.mAdminMap.values());
 
